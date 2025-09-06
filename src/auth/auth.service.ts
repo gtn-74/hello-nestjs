8a368acb2d76eb0ba9da4d29bd529e934a995580
gtn-74 <gtn.r74@gmail.com>
2025-09-06 21:15:02 +0900

    env:hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,26 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable } from '@nestjs/common';
 import { User } from 'generated/prisma';
+import bcrypt from 'node_modules/bcryptjs';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   // private は、このクラスのみでアクセスできる的な意味らしい
   constructor(private readonly prismaService: PrismaService) {}
+
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, status } = createUserDto;
+
+    // passwordのハッシュ化
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // statusどうするの??
     return await this.prismaService.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
         status,
       },
     });
